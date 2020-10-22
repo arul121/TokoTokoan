bd58d691c646c3794033adfabe6bafe281d1484e
arul121 <55161139+arul121@users.noreply.github.com>
2020-10-22 11:29:36 +0700

    Update cari-barang.php

--- admin/cari-barang.php
+++ admin/cari-barang.php
@@ -77,7 +77,7 @@ $nama = ( isset($_SESSION['user']) ) ? $_SESSION['user'] : '';
 		                    	<div class="background" style="margin-bottom:-15%;">
 		                    		<img src="../images/bg1.jpg">
 		                    	</div>
-		                		<span class="white-text name"><?php echo $nama; ?><i class="material-icons left">account_circle</i></span>
+		                		<span class="white-text name"><?php print $nama; ?><i class="material-icons left">account_circle</i></span>
 		                	</div>
 		                </li>
 		                
